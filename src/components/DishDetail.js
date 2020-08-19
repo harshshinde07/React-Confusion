c02b6c7a174f19925bdf728e4c2f2c127550b0a0
Harsh <harshshinde07@gmail.com>
2020-08-19 16:45:28 +0530

    container and presentational components

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -26,26 +26,29 @@ class DishDetail extends Component {
   }
 
   render() {
+    if (this.props.dish == null) return <div></div>;
     return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg
-              top
-              src={this.props.dish.image}
-              alt={this.props.dish.name}
-            />
-            <CardBody>
-              <CardTitle>{this.props.dish.name}</CardTitle>
-              <CardText>{this.props.dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <h4>Comments</h4>
-          <ul className="list-unstyled pl-0">
-            {this.renderComments(this.props.dish.comments)}
-          </ul>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <Card>
+              <CardImg
+                top
+                src={this.props.dish.image}
+                alt={this.props.dish.name}
+              />
+              <CardBody>
+                <CardTitle>{this.props.dish.name}</CardTitle>
+                <CardText>{this.props.dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled pl-0">
+              {this.renderComments(this.props.dish.comments)}
+            </ul>
+          </div>
         </div>
       </div>
     );
