996328fa3d652a5654101fa40bc867d5bd3887ed
Harsh <harshshinde07@gmail.com>
2020-08-26 12:34:03 +0530

    add react animation components to home and dish detail cards

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -19,6 +19,7 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./Loading";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -141,33 +142,41 @@ class CommentForm extends Component {
 }
 
 function RenderComments({ comments, postComment, dishId }) {
-  const cmts = comments.map((comment) => {
+  return comments.map((comment) => {
     return (
-      <li key={comment.id}>
-        <div>{comment.comment}</div>
-        <div style={{ margin: "1rem" }}>
-          -- {comment.author},{" "}
-          {new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "2-digit",
-          }).format(new Date(comment.date))}
-        </div>
-      </li>
+      <Fade in>
+        <li key={comment.id}>
+          <div>{comment.comment}</div>
+          <div style={{ margin: "1rem" }}>
+            -- {comment.author},{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(comment.date))}
+          </div>
+        </li>
+      </Fade>
     );
   });
-  return cmts;
 }
 
 function RenderDish({ dish }) {
   return (
-    <Card>
-      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-      <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-      </CardBody>
-    </Card>
+    <FadeTransform
+      in
+      transformProps={{
+        exitTransform: "scale(0.5) translateY(-50%)",
+      }}
+    >
+      <Card>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
